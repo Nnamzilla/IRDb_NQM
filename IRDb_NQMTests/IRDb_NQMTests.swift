c067449fcbbc1fcb754d8b785d4c2ca5a195abe2
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-03-18 08:05:19 -0500

    Initial Commit

---
+++ IRDb_NQMTests/IRDb_NQMTests.swift
@@ -0,0 +1,34 @@
+//
+//  IRDb_NQMTests.swift
+//  IRDb_NQMTests
+//
+//  Created by Nnamdi Massally on 3/18/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import XCTest
+@testable import IRDb_NQM
+
+class IRDb_NQMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
