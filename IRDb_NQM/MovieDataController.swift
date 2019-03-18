f7eda9f1d2a088e246c0d9255be43e54814220a3
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-03-18 08:36:35 -0500

    Starting this project, but I'm not very sure how any of this is supposed to go

---
+++ IRDb_NQM/MovieDataController.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDataController.swift
+//  IRDb_NQM
+//
+//  Created by Nnamdi Massally on 3/18/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import UIKit
+
+class MovieDataController: NSObject {
+
+}
---
+++ IRDb_NQM/MovieDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDataModel.swift
+//  IRDb_NQM
+//
+//  Created by Nnamdi Massally on 3/18/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import UIKit
+
+class MovieDataModel: NSObject {
+
+}
